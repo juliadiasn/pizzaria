d53810b22044a0d5edcf55e2f137a12209166225
Júlia Dias <79487857+juliadiasn@users.noreply.github.com>
2025-01-17 07:11:04 -0300

    Update dbConfig.mjs

--- database/dbConfig.mjs
+++ database/dbConfig.mjs
@@ -3,8 +3,10 @@ import { Sequelize } from 'sequelize';
 const conexao = new Sequelize({
   database: 'pizzaria',
   username: 'root',
-  password: 'root',
-  dialect: 'mysql',
+  password: '7EUQWzVuKvsXwW1KBjiZOfcRsYoevFoQ',
+  dialect: 'postgres',
+host: 'dpg-cu52o83tq21c73dvv0hg-a',
+port: '5432'
 });
 
 export default conexao;
